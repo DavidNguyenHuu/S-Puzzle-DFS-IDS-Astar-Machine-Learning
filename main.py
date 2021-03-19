ccc17095b3e56d2fe7dce6b929e365fd1651f74b
mikesarkis <59941108+mikesarkis@users.noreply.github.com>
2021-03-19 18:23:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import time
+from typing import Tuple
+
+
+class node:
+    def _int_(self, state, parent, operation, depth, cost):
+        self.state = state    # should consist of a list or tuple to represent the state we are currently in
+        self.parent = parent  # the other parameters are to keep find the path and the depth and the cost
+        self.operation = operation
+        self.depth = depth
+        self.cost = cost
+
+
+def create_node(state, parent, operation, depth, cost):
+    return node(state, parent, operation, depth, cost)
+
+
+def move_up(state, number_index):   # we will pass the state along with the index of the number that we want to move up
+    new_state = state  # we will copy the current state then we will swap
+    index = new_state.index(number_index)  # the number will indicate the index that we want to swap with it's neighbors
+    if index not in [1, 2, 3]:     # if the index is not in the upper part of the puzzle then swap with the upper index
+        temp = new_state[index - 3]
+        new_state[index - 3] = new_state[index]
+        new_state[index] = temp
+        return new_state    # after we swap we return a new state with a new order of numbers
+    else:
+        return None
+
+
+def move_down(state, number_index):   # the other moving functions will work in the same way
+    new_state = state
+    index = new_state.index(number_index)
+    if index not in [7, 8, 9]:
+        temp = new_state[index + 3]
+        new_state[index + 3] = new_state[index]
+        new_state[index] = temp
+        return new_state
+    else:
+        return None
+
+
+def move_left(state, number_index):
+    new_state = state
+    index = new_state.index(number_index)
+    if index not in [1, 4, 7]:
+        temp = new_state[index - 1]
+        new_state[index - 1] = new_state[index]
+        new_state[index] = temp
+        return new_state
+    else:
+        return None
+
+
+def move_right(state, number_index):
+    new_state = state
+    index = new_state.index(number_index)
+    if index not in [3, 6, 9]:
+        temp = new_state[index + 1]
+        new_state[index + 1] = new_state[index]
+        new_state[index] = temp
+        return new_state
+    else:
+        return None
+
+
+def search_node(node, number):  # search the node by applying all the moves and add the child states to a list
+    child_nodes = [create_node(move_right(node.state, number), node, "move right", node.depth + 1, 0),
+                   create_node(move_down(node.state, number), node, "move down", node.depth + 1, 0),
+                   create_node(move_up(node.state, number), node, "move up", node.depth + 1, 0),
+                   create_node(move_left(node.state, number), node, "move right", node.depth + 1, 0)]
+    for node in child_nodes:
+        if node.index is None:
+            child_nodes.remove(node)
+    return child_nodes  # return a list of child nodes which we will search next if we didn't reach the goal state
+
+
+
+
+
