6c79034c069c2026c02e91851efba20215f27001
mikesarkis <59941108+mikesarkis@users.noreply.github.com>
2021-03-25 16:54:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,80 @@
 import time
 from typing import Tuple
 
+
 class Node:
-    def __init__(self, state, parent, operation, depth, cost):
+    def __init__(self, state, parent, depth, cost):
         self.state = state  # should consist of a list or tuple to represent the state we are currently in
         self.parent = parent  # the other parameters are to keep find the path and the depth and the cost
-        self.operation = operation
         self.depth = depth
         self.cost = cost
 
 
-def create_node(state, parent, operation, depth, cost):
-    node1 = Node(state, parent, operation, depth, cost)
+def create_node(state, parent, depth, cost):
+    node1 = Node(state, parent, depth, cost)
     return node1
 
 
-def move_up(state, index):  # we will pass the state along with the index of the number that we want to move up
+def move_up(state, row, column):  # we will pass the state along with the index of the number that we want to move up
     new_state = state  # we will copy the current state then we will swap
-    if index not in [0, 1, 2]:  # if the index is not in the upper part of the puzzle then swap with the upper index
-        temp = new_state[index - 3]
-        new_state[index - 3] = new_state[index]
-        new_state[index] = temp
+    if row != 0:  # if the index is not in the upper part of the puzzle then swap with the upper index
+        temp = new_state[row - 1][column]
+        new_state[row - 1][column] = new_state[row][column]
+        new_state[row][column] = temp
         return new_state  # after we swap we return a new state with a new order of numbers
     else:
         return None
 
 
-def move_down(state, index):  # the other moving functions will work in the same way
+def move_down(state, row, column):  # the other moving functions will work in the same way
     new_state = state
-    if index not in [6, 7, 8]:
-        temp = new_state[index + 3]
-        new_state[index + 3] = new_state[index]
-        new_state[index] = temp
+    if row != 2:
+        temp = new_state[row + 1][column]
+        new_state[row + 1][column] = new_state[row][column]
+        new_state[row][column] = temp
         return new_state
     else:
         return None
 
 
-def move_left(state, index):
+def move_left(state, row, column):
     new_state = state
-    if index not in [0, 3, 6]:
-        temp = new_state[index - 1]
-        new_state[index - 1] = new_state[index]
-        new_state[index] = temp
+    if column != 0:
+        temp = new_state[row][column - 1]
+        new_state[row][column - 1] = new_state[row][column]
+        new_state[row][column] = temp
         return new_state
     else:
         return None
 
 
-def move_right(state, index):
+def move_right(state, row, column):
     new_state = state
-    if index not in [2, 5, 8]:
-        temp = new_state[index + 1]
-        new_state[index + 1] = new_state[index]
-        new_state[index] = temp
+    if column != 2:
+        temp = new_state[row][column + 1]
+        new_state[row][column + 1] = new_state[row][column]
+        new_state[row][column] = temp
         return new_state
     else:
         return None
 
 
-def search_node(Node, index):  # search the node by applying all the moves and add the child states to a list
-    child_nodes = [create_node(move_right(Node.state, index), Node, "move right", Node.depth + 1, 0),
-                   create_node(move_down(Node.state, index), Node, "move down", Node.depth + 1, 0),
-                   create_node(move_up(Node.state, index), Node, "move up", Node.depth + 1, 0),
-                   create_node(move_left(Node.state, index), Node, "move right", Node.depth + 1, 0)]
+def search_node(Node, rwo, column):  # search the node by applying all the moves and add the child states to a list
+    child_nodes = [create_node(move_right(Node.state, rwo, column), Node, Node.depth + 1, 0),
+                   create_node(move_down(Node.state, rwo, column), Node, Node.depth + 1, 0),
+                   create_node(move_up(Node.state, rwo, column), Node, Node.depth + 1, 0),
+                   create_node(move_left(Node.state, rwo, column), Node, Node.depth + 1, 0)]
     for Node in child_nodes:
         if Node.state is None:  # filtering nodes that have wrong states
             child_nodes.remove(Node)
     return child_nodes  # return a list of child nodes which we will search next if we didn't reach the goal state
 
 
-def DFS(start, goal, search_path, start_timer):
-    nodes_list = [start]  # we will add the first state node to our nodes list
+def DFS(start, goal, search, start_timer):
+    nodes_list = []  # we will add the first state node to our nodes list
+    nodes_list.append(start)
     solution_path = []  # we will use this list to backtrack the solution path
-    search_path.append(start.state)  # we will add the node to the search path
+    search.append(start.state)  # we will add the node to the search path
     while len(nodes_list) != 0:  # quit if we don't have any nodes left to search
         if start_timer == start_timer + 60:  # quit when timeout
             return "no solution"
@@ -83,14 +84,16 @@ def DFS(start, goal, search_path, start_timer):
                 while start.parent is not None:  # when we reach the original start node we will stop
                     solution_path.append(start.parent.state)  # we will add the parent node to the path
                 start = start.parent  # we will add the parent of the parent node
-                return solution_path, search_path  # we will return the solution path with the search path
+                print("the solution path for DFS :", solution_path)
+                return search  # we will return the solution path with the search path
             else:
                 nodes_list.remove(start)  # we will remove the node that we just checked
-                for index in start.state:  # for each index in the state of the current node we will apply the search
-                    child_list = search_node(start, start.state.index(index))  # we will add the results to a new list
+                for i in range(len(start.state)):
+                    for j in range(len(start.state[i])):
+                        child_list = search_node(start, i, j)  # we will add the results to a new list
                     for node in nodes_list:  # we will search each node in the child list
-                        search_path.append(node)
-                        DFS(node.state, goal, start_timer)
+                        search.append(node)
+                        DFS(node, goal, search, start_timer)
 
 
 
@@ -113,13 +116,15 @@ def IDS(start, goal, search_path, max_depth):  # we will pass start node, goal n
             False
             
 def main():
-    goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    initial_state = [6, 1, 2, 7, 8, 3, 5, 4, 9]
-    start = create_node(initial_state, None, None, 0, 0)
-    solution = []
-    search = []
+    goal_state = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+    initial_state = ((6, 1, 2), (7, 8, 3), (5, 4, 9))
+    goal_state_list = [list(i) for i in goal_state]
+    initial_state_list = [list(i) for i in initial_state]
+    start = create_node(initial_state_list, None, 0, 0)
     timer = time.time()
-    solution, search = DFS(start, goal_state, search, timer)
+    search = []
+    search = DFS(start, goal_state_list, search, timer)
+    print("the search path for DFS :", search)
 
 
 if __name__ == '__main__':
